f184c94d26519c52c02c6dca8d0b8192d88cc080
Jesus <jesusgp72021@gmail.com>
2026-02-02 13:16:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // 🔹 Configuración Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "escueladominical.firebaseapp.com",
-  projectId: "escueladominical",
-  storageBucket: "escueladominical.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "1:XXXX:web:XXXX"
+  apiKey: "AIzaSyDNd6f8zriP3U-1-zLTD0e_c45H73lEg1o",
+  authDomain: "escueladominicalreydereyes.firebaseapp.com",
+  projectId: "escueladominicalreydereyes",
+  storageBucket: "escueladominicalreydereyes.firebasestorage.app",
+  messagingSenderId: "891424130656",
+  appId: "1:891424130656:web:8b0e92dd4cf00ab40ce505"
 };
 
 firebase.initializeApp(firebaseConfig);
