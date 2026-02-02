6aec3c1adaaee96b7cdc02ce8c33bb7cc9d8ab63
Jesus <jesusgp72021@gmail.com>
2026-02-02 13:44:02 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
-// Configura tu proyecto de Firebase aquí
 const firebaseConfig = {
-  apiKey: "AIzaSyDNd6f8zriP3U-1-zLTD0e_c45H73lEg1o",
+ apiKey: "AIzaSyDNd6f8zriP3U-1-zLTD0e_c45H73lEg1o",
   authDomain: "escueladominicalreydereyes.firebaseapp.com",
   projectId: "escueladominicalreydereyes",
   storageBucket: "escueladominicalreydereyes.firebasestorage.app",
   messagingSenderId: "891424130656",
-  appId: "1:891424130656:web:8b0e92dd4cf00ab40ce505"
+  appId: "1:891424130656:web:8b0e92dd4cf00ab40ce505",
+
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
