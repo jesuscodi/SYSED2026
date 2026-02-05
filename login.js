51ad0bbae6e77296b9d4b057368237d96695d3e9
Jesus <jesusgp72021@gmail.com>
2026-02-05 13:14:32 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,24 +1,44 @@
-// login.js
-import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword }
+import { auth, db } from "./firebase.js";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword }
 from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-const btn = document.getElementById("btnLogin");
+import { doc, setDoc }
+from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
 const msg = document.getElementById("mensaje");
 
-btn.addEventListener("click", async () => {
-    const email = email.value;
-    const password = password.value;
-
-    if (!email || !password) {
-        msg.textContent = "Completa todos los campos";
-        return;
-    }
-
+// LOGIN
+document.getElementById("btnLogin").onclick = async () => {
     try {
-        await signInWithEmailAndPassword(auth, email, password);
+        await signInWithEmailAndPassword(
+            auth,
+            email.value,
+            password.value
+        );
         window.location.href = "dashboard.html";
     } catch (e) {
-        msg.textContent = "Correo o contraseña incorrectos";
+        msg.textContent = "Error al ingresar";
     }
-});
+};
+
+// REGISTRO
+document.getElementById("btnRegistro").onclick = async () => {
+    try {
+        const cred = await createUserWithEmailAndPassword(
+            auth,
+            email.value,
+            password.value
+        );
+
+        // Guardar datos en Firestore
+        await setDoc(doc(db, "usuarios", cred.user.uid), {
+            email: email.value,
+            rol: rol.value,
+            fecha: new Date()
+        });
+
+        msg.textContent = "Usuario creado correctamente";
+    } catch (e) {
+        msg.textContent = "Error al crear usuario";
+    }
+};
